795b1d013cd5ce5c7c62a429a1a94a0c3178dd7b
kwamenaabew <davidabewbaidoo@gmail.com>
2023-03-28 18:51:37 +0000

    0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+   empty class Square that defines a square
+
+"""
+
+
+class Square:
+    """Empty class Square that defines a square
+
+    Attributes:
+        empty
+    """
+    pass
