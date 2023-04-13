762f328cbb05a45fc68eb385ceef08216d5fa528
kwamenaabew <davidabewbaidoo@gmail.com>
2023-04-13 23:18:38 +0000

    first commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+function lookup
+"""
+
+
+def lookup(obj):
+    """
+    Returns all objects in an objects dictionary as a list
+    """
+    return dir(obj)
