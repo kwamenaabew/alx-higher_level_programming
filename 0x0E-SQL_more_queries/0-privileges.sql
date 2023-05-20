047a134116fa9d1f1832207fd69e6cc95ea2a9d1
kwamenaabew <davidabewbaidoo@gmail.com>
2023-05-20 08:44:27 +0000

    first commit

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,4 @@
+-- List the privileges using SHOW GRANTS
+-- DML QUERY for the list all privileges for user_0d_1 and user_0d_2 in localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
